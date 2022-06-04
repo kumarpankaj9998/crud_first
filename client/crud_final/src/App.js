8d2b8c62a0b4db9e4ba392f522b4f547ef94962e
Pankaj kumar <73705595+kumarpankaj9998@users.noreply.github.com>
2022-06-04 14:03:08 +0530

    commit3

--- client/crud_final/src/App.js
+++ client/crud_final/src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React ,{useState} from 'react';
+import Axios from 'axios';
 import './App.css';
 
 function App() {
+  const [name,setName] = useState('');
+  const [roll,setRoll]=useState(0);
+   
+  const addtoBack=()=>{
+   Axios.post("http://localhost:3001/insert",{studentName:name,studentRoll:roll}); 
+  };
+   
   return (
     <div className="App">
       <h1>FIRST TIME PLAYING WITH THIS MODAL</h1>
        <label>Name of the student</label> 
-       <input type="text" className="sizer"/>
+       <input type="text" className="sizer" onclick={(event)=>{setName(event.target.value)}}/>
        <label>Roll no:</label>
-       <input type="number" className="sizer"/>
-       <button type="submit"  className="sizer"  >SUBMIT </button>
+       <input type="number" className="sizer" onclick={(event)=>{setName(event.target.value)}}/>
+       <button type="submit"  className="sizer" onclick={addtoBack}>SUBMIT </button>
     </div>
     
 
