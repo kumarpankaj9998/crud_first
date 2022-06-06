ddd1be2709900fa0decb9ca626d1a5d17f482187
Pankaj kumar <73705595+kumarpankaj9998@users.noreply.github.com>
2022-06-06 19:20:54 +0530

    commit5

--- client/crud_final/src/App.js
+++ client/crud_final/src/App.js
@@ -5,14 +5,32 @@ import './App.css';
 function App() {
   const [name,setName] = useState('');
   const [roll,setRoll]=useState(0);
+  const [newName,setNewName]=useState('');
+  const [Stu,setStudent] = useState([]);
+
+   
+  
   useEffect(()=>{ 
-    Axios.get("http://localhost:3001/read").then((response)=>{console.log(response);})
+  
+    Axios.get("http://localhost:3001/read").then((response)=>{
+      
+      setStudent(response.data);
+
+      
+      });
   },[])
   const addtoBack=()=>{
-    console.log(name);
+    
    Axios.post("http://localhost:3001/insert",{name:name,roll:roll}); 
-  
+   console.log(name);
   };
+  const updateName=(id)=>{console.log(newName); 
+    Axios.put("http://localhost:3001/update",{id:id,newName:newName});
+  }
+  const deleteName=(id)=>{
+    console.log(id);
+    Axios.delete(`http://localhost:3001/delete/${id}`);
+  }
    
   return (
     <div className="App">
@@ -22,7 +40,17 @@ function App() {
        <label>Roll no:</label>
        <input type="number" className="sizer" onChange={(event)=>{setRoll(event.target.value)}}/>
        <button type="submit"  className="sizer" onClick={addtoBack}>SUBMIT </button>
+       
+       <h1>student detail's</h1>
+       { Stu.map((val,key) => {
+      return <div className="stu"><h1 >{val.studentName}</h1> <h1 >{val.studentRoll}</h1>
+         <input type="text" placeholder="new value" onChange={(event)=>setNewName(event.target.value)}/> <button onClick={()=>{updateName(val._id)}}>update</button><button onClick={()=>{deleteName(val._id)}} >delete</button></div>
+         
+      
+    })}
+   
     </div>
+    // 
     
 
   );
--- server/index.js
+++ server/index.js
@@ -4,29 +4,50 @@ const cors=require('cors')
 
 const app= express()
 const studentModel=require('./modals/student')
+const { update } = require('./modals/student')
 
 app.use(express.json())
 app.use(cors())
 
 mongoose.connect("mongodb+srv://userName:7891216649a@crud.8byar.mongodb.net/student?retryWrites=true&w=majority",{useNewUrlParser: true});
 
-app.get("/read",async(req,res)=>{
-    // const StudentName = req.body.name;
-    // const StudentRoll = req.body.roll;
-    // console.log(StudentName,StudentRoll);
-    // const student1 =new studentModel({studentName:StudentName, studentRoll:StudentRoll})
-   
-
-    // try{
-    //     const res1 = await student1.save();
-
-    //     console.log("Data entered", res1)
-
-    //     }catch(e){console.log(e);};
-    studentModel.find({},(err,result)=>{
-        if(err){res.send(err);}
-        res.send(result);
-    });
+app.get("/read",async (req, res)=>{
+  studentModel.find({},(err,result)=>{
+    if(err){res.send(err);}
+    res.send(result);
+  });
 });
+app.post("/insert",async(req,res)=>{
+   const StudentName = req.body.name;
+    const StudentRoll = req.body.roll;
+    console.log(StudentName,StudentRoll);
+    const student1 =new studentModel({studentName:StudentName, studentRoll:StudentRoll})
+try { 
+  await student1.save();
+  res.send("data inserted!");
+}catch(err){
+  console.log(err);
+}
+})
+
+app.put("/update",async(req,res)=>{
+   
+    const StudentNewName = req.body.newName;
+    const id = req.body.id;
+    try{
+      await  studentModel.findById(id,(err,updatedName)=>{
+            updatedName.studentName=StudentNewName;
+            updatedName.save();
+            res.send("update");
+        })
+
+        }catch(err){console.log(err);};
+});
+app.delete("/delete/:id",async (req, res)=>{
+const id1= req.params.id;
+await studentModel.findByIdAndRemove(id1).exec()
+res.send("deleted");
+})
+
 
 app.listen(3001,()=>{console.log("server running on port 3001");});
